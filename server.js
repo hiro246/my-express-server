da49fc90a6f30cdde96747661483b83cd5625ec6
hiro246 <roki_stereo@yahoo.co.jp>
2020-11-04 16:11:37 +0900

    Install nodemon & Add app.get

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("Hello, World!");
+});
+
+app.get("/contact", function(req, res){
+    res.send("contact:gmail");
+});
+
+app.get("/about", function(req, res){
+    res.send("私は山田です");
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("<ul><li>ビール</li><li>おつまみ</li></ul>");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
